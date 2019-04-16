51c21371f277690ef631439600d014b18997d19d
littlefxc <12741723+littlefxc@users.noreply.github.com>
2019-04-16 23:47:13 +0800

    fix(learn-spring-security/auth-server):oauth2 + zuul
    ResourceOwnerPasswordResourceDetails 的使用

--- learn-spring-security/spring-security-oauth2/gate-zuul/src/main/java/com/fengxuechao/examples/sso/gate/OauthGateZuulApp.java
+++ learn-spring-security/spring-security-oauth2/gate-zuul/src/main/java/com/fengxuechao/examples/sso/gate/OauthGateZuulApp.java
@@ -1,9 +1,23 @@
 package com.fengxuechao.examples.sso.gate;
 
+import com.netflix.discovery.converters.Auto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.security.oauth2.client.OAuth2LoadBalancerClientAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.security.oauth2.client.token.grant.password.ResourceOwnerPasswordAccessTokenProvider;
+import org.springframework.security.oauth2.client.token.grant.password.ResourceOwnerPasswordResourceDetails;
+import org.springframework.security.oauth2.common.AuthenticationScheme;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
 
 /**
  * @author fengxuechao
@@ -12,9 +26,43 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @EnableZuulProxy
 @EnableDiscoveryClient
 @SpringBootApplication
+@RestController
 public class OauthGateZuulApp {
 
     public static void main(String[] args) {
         SpringApplication.run(OauthGateZuulApp.class, args);
+    }
+
+    @RequestMapping("/test")
+    public String test() {
+        restTemplate.getAccessToken();
+        return "test";
+    }
+
+    @Autowired
+    private OAuth2RestTemplate restTemplate;
+
+    @Bean
+    public OAuth2RestTemplate restTemplate() {
+        OAuth2RestTemplate restTemplate = new OAuth2RestTemplate(resource());
+        restTemplate.setAccessTokenProvider(new ResourceOwnerPasswordAccessTokenProvider());
+        return restTemplate;
+    }
+
+    private OAuth2ProtectedResourceDetails resource() {
+
+        ResourceOwnerPasswordResourceDetails resource = new ResourceOwnerPasswordResourceDetails();
+        resource.setAccessTokenUri("http://localhost:8081/oauth/token");
+
+        resource.setClientId("client_1");
+        resource.setClientSecret("123456");
+
+        resource.setGrantType("password");
+        resource.setScope(Arrays.asList("all"));
+
+        resource.setUsername("admin");
+        resource.setPassword("admin");
+
+        return resource;
     }
 }
--- learn-spring-security/spring-security-oauth2/gate-zuul/src/main/java/com/fengxuechao/examples/sso/gate/SecurityConfig.java
+++ learn-spring-security/spring-security-oauth2/gate-zuul/src/main/java/com/fengxuechao/examples/sso/gate/SecurityConfig.java
@@ -1,14 +1,9 @@
 package com.fengxuechao.examples.sso.gate;
 
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.client.OAuth2ClientContext;
-import org.springframework.security.oauth2.client.OAuth2RestTemplate;
-import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 
 /**
@@ -31,12 +26,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();  // 禁用csrf
-    }
-
-    @LoadBalanced
-    @Bean
-    public OAuth2RestTemplate oauth2RestTemplate(
-            OAuth2ClientContext oAuth2ClientContext, OAuth2ProtectedResourceDetails details) {
-        return new OAuth2RestTemplate(details, oAuth2ClientContext);
+//        http.authorizeRequests().antMatchers("/test").authenticated();
     }
 }
