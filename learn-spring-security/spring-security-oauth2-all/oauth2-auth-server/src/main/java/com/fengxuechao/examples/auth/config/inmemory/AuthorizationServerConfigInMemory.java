2fe974ead0904ffc1105dfa19dac1c53f58a3703
littlefxc <12741723+littlefxc@users.noreply.github.com>
2019-05-17 16:23:22 +0800

    feat(sprig-security-oauth2-all):自定义授权模式

--- learn-spring-security/spring-security-oauth2-all/oauth2-auth-server/src/main/java/com/fengxuechao/examples/auth/config/inmemory/AuthorizationServerConfigInMemory.java
+++ learn-spring-security/spring-security-oauth2-all/oauth2-auth-server/src/main/java/com/fengxuechao/examples/auth/config/inmemory/AuthorizationServerConfigInMemory.java
@@ -1,28 +1,43 @@
 package com.fengxuechao.examples.auth.config.inmemory;
 
 import com.fengxuechao.examples.auth.config.CustomTokenEnhancer;
+import com.fengxuechao.examples.auth.provider.token.SmsCodeTokenGranter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.core.userdetails.UserDetailsByNameServiceWrapper;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.oauth2.provider.*;
 import org.springframework.security.oauth2.provider.approval.ApprovalStore;
 import org.springframework.security.oauth2.provider.approval.TokenApprovalStore;
-import org.springframework.security.oauth2.provider.token.TokenEnhancer;
-import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
-import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.client.ClientCredentialsTokenGranter;
+import org.springframework.security.oauth2.provider.code.AuthorizationCodeServices;
+import org.springframework.security.oauth2.provider.code.AuthorizationCodeTokenGranter;
+import org.springframework.security.oauth2.provider.code.InMemoryAuthorizationCodeServices;
+import org.springframework.security.oauth2.provider.implicit.ImplicitTokenGranter;
+import org.springframework.security.oauth2.provider.password.ResourceOwnerPasswordTokenGranter;
+import org.springframework.security.oauth2.provider.refresh.RefreshTokenGranter;
+import org.springframework.security.oauth2.provider.request.DefaultOAuth2RequestFactory;
+import org.springframework.security.oauth2.provider.token.*;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationProvider;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
 import org.springframework.util.Base64Utils;
 
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author fengxuechao
@@ -50,6 +65,26 @@ public class AuthorizationServerConfigInMemory extends AuthorizationServerConfig
     @Autowired
     private ApprovalStore approvalStore;
 
+    private AuthorizationCodeServices authorizationCodeServices;
+
+    private boolean reuseRefreshToken = true;
+
+    private AuthorizationServerTokenServices tokenServices;
+
+    private TokenGranter tokenGranter;
+
+    @Bean
+    public TokenApprovalStore tokenApprovalStore() {
+        TokenApprovalStore approvalStore = new TokenApprovalStore();
+        approvalStore.setTokenStore(tokenStore);
+        return approvalStore;
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new InMemoryTokenStore();
+    }
+
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
         security
@@ -66,7 +101,7 @@ public class AuthorizationServerConfigInMemory extends AuthorizationServerConfig
                 .secret("123456")
                 .accessTokenValiditySeconds(120)
                 .refreshTokenValiditySeconds(3600)
-                .authorizedGrantTypes("client_credentials", "password", "authorization_code", "implicit", "refresh_token")
+                .authorizedGrantTypes("client_credentials", "password", "authorization_code", "implicit", "refresh_token", "sms_code")
                 .redirectUris("https://www.baidu.com")
                 .scopes("read")
                 .authorities("ROLE_USER")
@@ -74,13 +109,17 @@ public class AuthorizationServerConfigInMemory extends AuthorizationServerConfig
         ;
     }
 
+    /**
+     * 认证服务器节点配置
+     *
+     * @param endpoints
+     * @throws Exception
+     */
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
-        tokenEnhancerChain.setTokenEnhancers(Collections.singletonList(tokenEnhancer()));
         endpoints
+                .tokenGranter(tokenGranter())
                 .tokenStore(tokenStore)
-                .tokenEnhancer(tokenEnhancerChain)
                 .approvalStore(approvalStore)
                 .userDetailsService(userDetailsService)
                 .authenticationManager(authenticationManager)
@@ -88,23 +127,102 @@ public class AuthorizationServerConfigInMemory extends AuthorizationServerConfig
     }
 
     /**
-     * Token 额外信息
+     * 授权模式
+     *
      * @return
      */
-    @Bean
-    public TokenEnhancer tokenEnhancer() {
-        return new CustomTokenEnhancer();
+    private TokenGranter tokenGranter() {
+        if (tokenGranter == null) {
+            tokenGranter = new TokenGranter() {
+                private CompositeTokenGranter delegate;
+
+                @Override
+                public OAuth2AccessToken grant(String grantType, TokenRequest tokenRequest) {
+                    if (delegate == null) {
+                        delegate = new CompositeTokenGranter(getDefaultTokenGranters());
+                    }
+                    return delegate.grant(grantType, tokenRequest);
+                }
+            };
+        }
+        return tokenGranter;
     }
 
-    @Bean
-    public TokenApprovalStore tokenApprovalStore() {
-        TokenApprovalStore approvalStore = new TokenApprovalStore();
-        approvalStore.setTokenStore(tokenStore);
-        return approvalStore;
+    private List<TokenGranter> getDefaultTokenGranters() {
+        AuthorizationServerTokenServices tokenServices = tokenServices();
+        AuthorizationCodeServices authorizationCodeServices = authorizationCodeServices();
+        OAuth2RequestFactory requestFactory = requestFactory();
+
+        List<TokenGranter> tokenGranters = new ArrayList<TokenGranter>();
+        tokenGranters.add(new AuthorizationCodeTokenGranter(tokenServices, authorizationCodeServices, clientDetailsService, requestFactory));
+        tokenGranters.add(new RefreshTokenGranter(tokenServices, clientDetailsService, requestFactory));
+        tokenGranters.add(new ImplicitTokenGranter(tokenServices, clientDetailsService, requestFactory));
+        tokenGranters.add(new ClientCredentialsTokenGranter(tokenServices, clientDetailsService, requestFactory));
+        if (authenticationManager != null) {
+            tokenGranters.add(new ResourceOwnerPasswordTokenGranter(authenticationManager, tokenServices, clientDetailsService, requestFactory));
+            tokenGranters.add(new SmsCodeTokenGranter(authenticationManager, tokenServices, clientDetailsService, requestFactory));
+        }
+        return tokenGranters;
     }
 
-    @Bean
-    public TokenStore tokenStore() {
-        return new InMemoryTokenStore();
+    private AuthorizationServerTokenServices tokenServices() {
+        if (tokenServices != null) {
+            return tokenServices;
+        }
+        this.tokenServices = createDefaultTokenServices();
+        return tokenServices;
     }
+
+    /**
+     * 授权码API
+     *
+     * @return
+     */
+    private AuthorizationCodeServices authorizationCodeServices() {
+        if (authorizationCodeServices == null) {
+            authorizationCodeServices = new InMemoryAuthorizationCodeServices();
+        }
+        return authorizationCodeServices;
+    }
+
+    /**
+     * OAuth2RequestFactory的默认实现，它初始化参数映射中的字段，
+     * 验证授权类型(grant_type)和范围(scope)，并使用客户端的默认值填充范围(scope)（如果缺少这些值）。
+     *
+     * @return
+     */
+    private OAuth2RequestFactory requestFactory() {
+        return new DefaultOAuth2RequestFactory(clientDetailsService);
+    }
+
+    private DefaultTokenServices createDefaultTokenServices() {
+        DefaultTokenServices tokenServices = new DefaultTokenServices();
+        tokenServices.setTokenStore(tokenStore);
+        tokenServices.setSupportRefreshToken(true);
+        tokenServices.setReuseRefreshToken(reuseRefreshToken);
+        tokenServices.setClientDetailsService(clientDetailsService);
+        tokenServices.setTokenEnhancer(tokenEnhancer());
+        addUserDetailsService(tokenServices, this.userDetailsService);
+        return tokenServices;
+    }
+
+    /**
+     * Token 额外信息
+     *
+     * @return
+     */
+    private TokenEnhancer tokenEnhancer() {
+        TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
+        tokenEnhancerChain.setTokenEnhancers(Arrays.<TokenEnhancer>asList(new CustomTokenEnhancer()));
+        return tokenEnhancerChain;
+    }
+
+    private void addUserDetailsService(DefaultTokenServices tokenServices, UserDetailsService userDetailsService) {
+        if (userDetailsService != null) {
+            PreAuthenticatedAuthenticationProvider provider = new PreAuthenticatedAuthenticationProvider();
+            provider.setPreAuthenticatedUserDetailsService(new UserDetailsByNameServiceWrapper<PreAuthenticatedAuthenticationToken>(userDetailsService));
+            tokenServices.setAuthenticationManager(new ProviderManager(Arrays.<AuthenticationProvider>asList(provider)));
+        }
+    }
+
 }
