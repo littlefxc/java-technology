ad39059523ea6ecb2237283420e51507385712c4
littlefxc <12741723+littlefxc@users.noreply.github.com>
2019-05-17 11:33:47 +0800

    fix(sprig-security-oauth2-all):资源服务器代码精简

--- learn-spring-security/spring-security-oauth2-all/oauth2-resource-server/src/main/java/com/fengxuechao/examples/res/ResourceServerConfigInJwt.java
+++ learn-spring-security/spring-security-oauth2-all/oauth2-resource-server/src/main/java/com/fengxuechao/examples/res/ResourceServerConfigInJwt.java
@@ -1,11 +1,35 @@
 package com.fengxuechao.examples.res;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.JwtAccessTokenConverterConfigurer;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.JwtAccessTokenConverterRestTemplateCustomizer;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.AnnotationAwareOrderComparator;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.token.TokenService;
+import org.springframework.security.crypto.codec.Base64;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.oauth2.provider.token.store.jwk.JwkTokenStore;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
 
 /**
  * @author fengxuechao
@@ -22,4 +46,38 @@ public class ResourceServerConfigInJwt extends ResourceServerConfigurerAdapter {
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().anyRequest().authenticated();
     }
+
+    /*@Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.tokenServices(tokenService());
+    }
+
+    @Autowired
+    private ResourceServerProperties resource;
+
+    @Primary
+    @Bean
+    public ResourceServerTokenServices tokenService() {
+        DefaultTokenServices services = new DefaultTokenServices();
+        services.setTokenStore(jwkTokenStore());
+        return services;
+    }
+
+    @Bean
+    public TokenStore jwkTokenStore() {
+        return new JwtTokenStore(jwtTokenEnhancer());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter jwtTokenEnhancer() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        String keyValue = this.resource.getJwt().getKeyValue();
+        if (StringUtils.hasText(keyValue) && !keyValue.startsWith("-----BEGIN")) {
+            converter.setSigningKey(keyValue);
+        }
+        if (keyValue != null) {
+            converter.setVerifierKey(keyValue);
+        }
+        return converter;
+    }*/
 }
--- learn-spring-security/spring-security-oauth2-all/oauth2-resource-server/src/main/java/com/fengxuechao/examples/res/ResourceServerConfigInMemory.java
+++ learn-spring-security/spring-security-oauth2-all/oauth2-resource-server/src/main/java/com/fengxuechao/examples/res/ResourceServerConfigInMemory.java
@@ -1,10 +1,15 @@
 package com.fengxuechao.examples.res;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
 
 /**
  * @author fengxuechao
@@ -20,5 +25,21 @@ public class ResourceServerConfigInMemory extends ResourceServerConfigurerAdapte
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().anyRequest().authenticated();
     }
+
+    /*@Autowired
+    private ResourceServerProperties resource;
+
+    @Primary
+    @Bean
+    public RemoteTokenServices tokenServices() {
+        RemoteTokenServices tokenServices = new RemoteTokenServices();
+        tokenServices.setCheckTokenEndpointUrl(resource.getUserInfoUri());
+        if (resource.isPreferTokenInfo()){
+            tokenServices.setCheckTokenEndpointUrl(resource.getTokenInfoUri());
+        }
+        tokenServices.setClientId(resource.getClientId());
+        tokenServices.setClientSecret(resource.getClientSecret());
+        return tokenServices;
+    }*/
 }
 
