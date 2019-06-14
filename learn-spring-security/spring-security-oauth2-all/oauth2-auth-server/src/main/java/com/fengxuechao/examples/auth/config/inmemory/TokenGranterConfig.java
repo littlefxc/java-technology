ab50fd79a79697d790070018e58d454d49f7e374
littlefxc <12741723+littlefxc@users.noreply.github.com>
2019-06-14 14:39:11 +0800

    chore(sprig-security-oauth2-all):TokenGranterConfig.java转移之inmemory中

--- learn-spring-security/spring-security-oauth2-all/oauth2-auth-server/src/main/java/com/fengxuechao/examples/auth/config/inmemory/TokenGranterConfig.java
+++ learn-spring-security/spring-security-oauth2-all/oauth2-auth-server/src/main/java/com/fengxuechao/examples/auth/config/inmemory/TokenGranterConfig.java
@@ -1,9 +1,10 @@
-package com.fengxuechao.examples.auth.config;
+package com.fengxuechao.examples.auth.config.inmemory;
 
 import com.fengxuechao.examples.auth.provider.token.SmsCodeTokenGranter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.ProviderManager;
@@ -19,7 +20,10 @@ import org.springframework.security.oauth2.provider.implicit.ImplicitTokenGrante
 import org.springframework.security.oauth2.provider.password.ResourceOwnerPasswordTokenGranter;
 import org.springframework.security.oauth2.provider.refresh.RefreshTokenGranter;
 import org.springframework.security.oauth2.provider.request.DefaultOAuth2RequestFactory;
-import org.springframework.security.oauth2.provider.token.*;
+import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationProvider;
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
 
@@ -33,6 +37,7 @@ import java.util.List;
  * @date 2019/5/17
  */
 @Configuration
+@Profile("inMemory")
 public class TokenGranterConfig {
 
     @Autowired
@@ -103,6 +108,11 @@ public class TokenGranterConfig {
         return tokenGranters;
     }
 
+    /**
+     * TokenServices
+     *
+     * @return
+     */
     private AuthorizationServerTokenServices tokenServices() {
         if (tokenServices != null) {
             return tokenServices;
@@ -133,6 +143,11 @@ public class TokenGranterConfig {
         return new DefaultOAuth2RequestFactory(clientDetailsService);
     }
 
+    /**
+     * 默认 TokenService
+     *
+     * @return
+     */
     private DefaultTokenServices createDefaultTokenServices() {
         DefaultTokenServices tokenServices = new DefaultTokenServices();
         tokenServices.setTokenStore(tokenStore);
@@ -144,6 +159,12 @@ public class TokenGranterConfig {
         return tokenServices;
     }
 
+    /**
+     * 添加预身份验证
+     *
+     * @param tokenServices
+     * @param userDetailsService
+     */
     private void addUserDetailsService(DefaultTokenServices tokenServices, UserDetailsService userDetailsService) {
         if (userDetailsService != null) {
             PreAuthenticatedAuthenticationProvider provider = new PreAuthenticatedAuthenticationProvider();
